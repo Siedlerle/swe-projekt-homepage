804cf62ecb48afcc70a9f60d5ac9e5f185c854a0
Fabian_Eilber <fabian.eilber01@gmail.com>
2023-03-12 13:34:44 +0100

    Private Abgabe Login überarbeitet.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import { Component,  ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,6 +15,8 @@ export class LoginComponent
   @ViewChild('nameInput') nameInputRef!: ElementRef<HTMLInputElement>;
   @ViewChild('passwdInput') passwdInputRef!: ElementRef<HTMLInputElement>;
 
+  loginFailed = false;
+
   name = '';
   password = '';
 
@@ -22,18 +25,21 @@ export class LoginComponent
 
   constructor(private router: Router) {}
 
-  onSubmit(): void {
+  onSubmit(event: Event): void {
+    event.preventDefault();
     this.name = this.nameInputRef.nativeElement.value;
     this.password = this.passwdInputRef.nativeElement.value;
 
     // Weiterleitung zur nächsten Komponente
     if (this.name == this.correctName && this.password == atob(this.correctPsswd)) {
       this.router.navigate(['/geschützte_Abgaben']);
+      this.loginFailed = false;
     } 
-    else if(this.name != '' && this.password != '')
-    {
-      this.router.navigate(['/login']);
+    else{
+      this.nameInputRef.nativeElement.value = '';
+      this.passwdInputRef.nativeElement.value = '';
+      this.loginFailed = true;
     }
   }
-}
 
+}
