5f609e9f61f441db9bf23817377217e50623cfd1
Fabian_Eilber <fabian.eilber01@gmail.com>
2023-03-12 19:09:13 +0100

    Abgaben Anschauen und Herunterladen Test.

--- src/app/abgaben/abgaben.component.ts
+++ src/app/abgaben/abgaben.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-abgaben',
   templateUrl: './abgaben.component.html',
   styleUrls: ['./abgaben.component.css']
 })
-export class AbgabenComponent {
 
+export class AbgabenComponent {
+  
 }
